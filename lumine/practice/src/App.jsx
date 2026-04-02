d3bec5666e4e36c626a76877bc78464bb596afd4
unsk1lled1 <nwjenj@gmail.com>
2026-04-02 22:08:51 +0500

    basa

---
+++ lumine/practice/src/App.jsx
@@ -0,0 +1,13 @@
+// TODO: подключи сюда роутер, страницы и компоненты
+// Подсматривай в LumineStore/client/src/App.jsx
+
+function App() {
+  return (
+    <div>
+      <h1>LumineStore — Practice</h1>
+      <p>Начни писать здесь!</p>
+    </div>
+  )
+}
+
+export default App
